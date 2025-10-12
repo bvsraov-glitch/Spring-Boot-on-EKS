a9ba6da7e754977072db98d80ba3c30ee3a3c97e
bvsraov-glitch <bvsrao.v@gmail.com>
2025-10-12 20:57:49 +0000

    added spring boot application

---
+++ SpringBootOnEKS/src/main/java/com/example/springbootoneks/SpringBootEksApplication.java
@@ -0,0 +1,27 @@
+package com.example.springbootoneks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@SpringBootApplication
+@RestController
+public class SpringBootEksApplication {
+
+    @GetMapping("/greetings")
+    public String message() {
+        return "Welcome to javatechie ! app deployed in AWS EKS";
+    }
+    
+    @RequestMapping("/")
+    public RedirectView redirectToGreetings() {
+        return new RedirectView("/greetings");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEksApplication.class, args);
+    }
+}
